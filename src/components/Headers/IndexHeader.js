d685d1dace8ccc9b0235fb5b627311aee6806114
zuri <desarrollo3@drsecurity.net>
2023-02-23 19:10:36 -0600

    inicial

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,52 @@
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+// core components
+
+function IndexHeader() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth > 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+
+  return (
+    <>
+      <div className="page-header clear-filter" filter-color="blue">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: "url(" + require("assets/img/edificio1.jpg") + ")"
+          }}
+          ref={pageHeader}
+        ></div>
+        <Container>
+          <div className="content-center brand">
+            <img
+              alt="..."
+              className="n-logo"
+              src={require("assets/img/logovip.png")}
+            ></img>
+            <h1 className="h1-seo">Vip Ingeniería.</h1>
+            <h3>Construya con VIP INGENIERÍA</h3>
+          </div>
+       
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
