9a845f50cfb52f6c4a3c75c6ece47e0cba118cb0
zuri <desarrollo3@drsecurity.net>
2023-03-10 14:08:16 -0600

    limpieza de codigo

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import "assets/demo/demo.css?v=1.5.0";
 import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
 // pages for this kit
 import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
 import LandingPage from "views/examples/LandingPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
 
